aee819d9d3ec77a5c96f04b2f11601908728e6a6
Jaron Pascal <28482704+jaronpascal@users.noreply.github.com>
2023-09-12 13:56:34 +0200

    Add issue

--- issue.js
+++ issue.js
@@ -1,31 +1,44 @@
-const sqlite3 = require('sqlite3').verbose();
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const app = express();
+const port = 3000;
 
-// Create a new SQLite database (or open an existing one)
+// Connect to SQLite database (or create a new one if it doesn't exist)
 const db = new sqlite3.Database('mydatabase.db');
 
-// Create a table
-db.serialize(() => {
-  db.run('CREATE TABLE users (id INT, name TEXT)');
-});
+// Middleware to parse JSON in request body
+app.use(express.json());
 
-// Insert data
-db.serialize(() => {
-  const stmt = db.prepare('INSERT INTO users VALUES (?, ?)');
-  stmt.run(1, 'Alice');
-  stmt.run(2, 'Bob');
-  stmt.finalize();
-});
-
-// Retrieve data
-db.serialize((fieldValue) => {
-  const value =  10 / fieldValue;
-  db.each("SELECT id, name FROM users WHERE Page = '" & value & "';", (err, row) => {
+// Route to get a list of items from the SQLite database
+app.get('/api/items', (req, res) => {
+  db.all('SELECT * FROM items', (err, rows) => {
     if (err) {
-      console.error(err.message);
+      res.status(500).json({ error: err.message });
+      return;
     }
-    console.log(row.id, row.name);
+    res.json(rows);
   });
 });
 
-// Close the database when done
-db.close();
+// Route to get a single item by ID from the SQLite database
+app.get('/api/items/:id', (req, res) => {
+  const itemId = req.params.id;
+
+  db.get('SELECT * FROM items WHERE id = ?', [itemId], (err, row) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    if (!row) {
+      res.status(404).json({ message: 'Item not found' });
+    } else {
+      res.json(row);
+    }
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
